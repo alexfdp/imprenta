a2c2b42c030a806ea08f93d42a2e2e2a59eb7b96
Alex Fajardo <alexf.124@hotmail.com>
2023-07-31 19:54:28 -0500

    Auto-generated commit [ci skip]

---
+++ assets/js/animacionReg.js
@@ -0,0 +1,16 @@
+let banderaVisibilidad = true;
+function functionVisibilidadPassword() {
+    var elemento = document.getElementById("icono-eye");
+    var inputPassword = document.getElementById("campo-password-valitaion");
+    if (banderaVisibilidad) {
+        elemento.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
+        inputPassword.type = "text";
+        banderaVisibilidad = false;
+    }
+    else {
+        elemento.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
+        inputPassword.type = "password";
+        banderaVisibilidad = true;
+    }
+
+}
